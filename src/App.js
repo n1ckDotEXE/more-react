6f00a1a1e267754462402f3ed710536fa67744d7
n1ckDotEXE <admin@nicholasnam.com>
2021-03-15 12:40:26 -0400

    Finishes App 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
-exort class App extends Component {
-	state = {};
+export class App extends Component {
+	state = {
+		input: "",
+		toggleSubmit: true,
+	};
+	onChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
 
+	handleSubmit = () => {
+		this.setState({
+			toggleSubmit: true,
+		});
+	};
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log("23 Should Component Update");
+
+		console.log(nextState);
+		if (nextState.input === "123") {
+			return false;
+		}
+
+		if (nextState.input === "hamster") {
+			return false;
+		}
+		return true;
+	}
 	render() {
 		return (
 			<div
@@ -10,7 +37,22 @@ exort class App extends Component {
 					marginTop: 10,
 					backgroundColor: this.state.backgroundColor,
 				}}
-			></div>
+			>
+				<div>
+					<input
+						type="text"
+						name="input"
+						onChange={this.onChange}
+						value={this.state.input}
+					/>
+				</div>
+				<br />
+				<br />
+				<button onClick={this.handleSubmit}>Submit</button>
+				<br />
+				<br />
+				{this.state.input}
+			</div>
 		);
 	}
 }
